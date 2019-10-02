1fa9eb190b857395bf43f57cee7bb42a93407385
ltran <tranln025@gmail.com>
2019-10-02 14:31:27 -0700

    Creates timer countdown functionality when Start button is clicked

--- js/app.js
+++ js/app.js
@@ -9,17 +9,36 @@ ctx.fillRect(0, 0, canvas.width, 50);
 // Stopwatch icon
 var stopwatch = new Image();
 stopwatch.addEventListener('load', function() {
-    console.log('test');
     ctx.drawImage(stopwatch, 10, 10, 27, 30);
 }, false);
 stopwatch.src = './images/stopwatch-solid.svg';
     // stopwatch source: https://fontawesome.com/icons/stopwatch?style=solid
 
 // Countdown Text
-let countdown = 30;
+let timeLeft = 30;
 ctx.fillStyle = "white";
 ctx.font = "25px Gloria Hallelujah";
-ctx.fillText(countdown + " seconds", 50, 35);
+ctx.fillText(timeLeft, 50, 35);
+
+// Timer Countdown Functions
+// Every second, reduce countdown by 1. When no time is left, stop the countdown, display score, offer "try again" button. On button click, restart countdown
+const startTimer = () => {
+    const timer = setInterval(()=>{    // setInterval ( function, amount of time )
+        timeLeft--;
+        console.log(timeLeft);
+        // Clear previous timer text and replace it each second with updated timeLeft
+        ctx.clearRect(40, 0, 50, 50);
+        ctx.fillStyle = 'rgb(255, 124, 110)';
+        ctx.fillRect(40, 0, 50, 50);
+        ctx.fillStyle = "white";
+        ctx.font = "25px Gloria Hallelujah";
+        ctx.fillText(timeLeft, 50, 35);
+        if (timeLeft === 0) {
+            clearInterval(timer);
+        }
+    }, 1000) 
+    return;
+}
 
 // Scoreboard
 let points = 0;
@@ -27,20 +46,14 @@ ctx.fillStyle = "white";
 ctx.font = "25px Gloria Hallelujah";
 ctx.fillText("Points: " + points, 570, 35);
 
-// Remove instruction box on button click
-$(`.closeInstructions`).on(`click`, () => {
-    console.log($(`.instructionsContainer`));
+// When Start, remove instructions and start game
+$(`.start`).on(`click`, () => {
+    console.log(`START has been clicked`);
     $(`.instructionsContainer`).removeClass(`zoomIn delay-1s`).addClass(`zoomOut`);
+    // TODO create start game fx
+    // TODO start countdown fx
+    startTimer();
 })
-
-
-// // Small Flat Zit
-// var flatZit = new Image();
-// flatZit.addEventListener('load', function() {
-//     console.log('test');
-//     ctx.drawImage(flatZit, 100, 100, 120, 90);
-// }, false);
-// flatZit.src = './images/flat-zit-unpopped.png';
 
 // // Big Popup Zit
 // var bigZit = new Image();
@@ -50,11 +63,27 @@ $(`.closeInstructions`).on(`click`, () => {
 // }, false);
 // bigZit.src = './images/big-zit.png';
 
-// Small Popup Zit
-var smallZit = new Image();
-smallZit.addEventListener('load', function() {
-    console.log('test');
-    ctx.drawImage(smallZit, 500, 300, 100, 50);
-}, false);
-smallZit.src = './images/small-zit.png';
+// // Small Popup Zit
+// var smallZit = new Image();
+// smallZit.addEventListener('load', function() {
+//     console.log('test');
+//     ctx.drawImage(smallZit, 500, 300, 100, 50);
+// }, false);
+// smallZit.src = './images/small-zit.png';
 
+// Small Flat Zit
+var flatZit = new Image();
+// Function to draw the zit. Start button triggers drawing. 
+// ctx.drawImage(flatZit, 100, 100, 110, 90);
+flatZit.src = './images/flat-zit-unpopped.png';
+
+// Randomize zit position
+const randomZit = () => {
+    let x = Math.random() * (canvas.width - 120) + 30;
+    let y = Math.random() * (canvas.height - 200) + 60;
+    flatZit.addEventListener('load', function() {
+        ctx.drawImage(flatZit, x, y, 110, 90);
+    }, false);
+};
+
+// in span of random time from 0 to 30 seconds, run random zit function
