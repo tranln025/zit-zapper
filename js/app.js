1a91f5732a46c18d2e9c5ba5307e22d843f78dc0
ltran <tranln025@gmail.com>
2019-10-03 00:45:07 -0700

    Adds animation for zit pop when clicked

--- js/app.js
+++ js/app.js
@@ -1,15 +1,17 @@
 
 // Timer Countdown Functions
-// Every second, reduce countdown by 1. When no time is left, stop the countdown, display score, offer "try again" button. On button click, restart countdown
+// Every second, reduce countdown by 1 while there is time left. At time=0, stop the countdown, display score, offer "try again" button. On button click, restart countdown
 let timeLeft = 30;
 const startTimer = () => {
-    const timer = setInterval(()=>{    // setInterval ( function, amount of time )
+    const timer = setInterval(() => {
         timeLeft --;
-        if (timeLeft === 0) {
-            clearInterval(timer);
-            // TODO insert "try again" button
+        if (timeLeft >= 0) {
+            updateTime();
+            if (timeLeft === 0) {
+                clearInterval(timer);
+                // TODO display score and "try again" button
+            }
         }
-        updateTime();
     }, 1000) 
 }
 
@@ -17,28 +19,20 @@ const updateTime = () => {
     $(`.countdown`).text(`${timeLeft} seconds`)
 }
 
-// When Start is clicked, remove instructions and start game
+// When Start is clicked, zoom out of instructions, remove from game screen, and start game
 $(`.start`).on(`click`, () => {
     console.log(`START has been clicked`);
     $(`.instructionsContainer`).removeClass(`zoomIn delay-1s`).addClass(`zoomOut`);
+    setTimeout(() => {
+        $(`.instructionsContainer`).remove();
+    }, 1000)
     // TODO create start game fx
     startTimer();
 })
 
 
 // // Randomize zit position and time interval
-// const randomZitPos = () => {
-//     let x = Math.random() * ($(`.gameScreen`).width - 120) + 30;
-//     let y = Math.random() * ($(`.gameScreen`).height - 200) + 60;
-//     $(`.gameScreen`).append(`<img class="ripeZit" src="./images/flat-zit-unpopped.png" alt="ripe pimple">`);
-// };
-
-// const randomTimeInterval = (Math.random() * 5000) + 1000;
-// const zitPopUp = () => {
-//     setInterval(randomZitPos(), randomTimeInterval);
-// }
-
-// Code for elements to appear sporadically and in various sizes. Source: http://jsfiddle.net/redler/QcUPk/8/
+// Source code for elements to appear sporadically and in various sizes: http://jsfiddle.net/redler/QcUPk/8/
 function zitAppear(){
     let divsize = (Math.round(Math.random()*100) + 50);
     $newZit = $('<img class="newZit" src="./images/flat-zit-unpopped.png" alt="ripe pimple">').css({
@@ -48,17 +42,25 @@ function zitAppear(){
     
     let posx = Math.random() * ($(`.gameScreen`).width() - divsize);
     let posy = Math.random() * (($(`.gameScreen`).height() - divsize) - 50) + 50;
-    let randDelay = Math.random() * 20000;
     
     $newZit.css({
         'position':'absolute',
         'left':posx+'px',
         'top':posy+'px',
         'display':'none'
-    }).appendTo('.gameScreen').fadeIn().delay(randDelay); 
+    }).appendTo('.gameScreen').fadeIn(); 
 }; 
 
-zitAppear();
+setInterval(() => {
+    let randInterval = Math.random() * 2000;
+    setTimeout(() => zitAppear(), randInterval);
+}, 2000);
 
-
-// when clicked, swap image of unpopped zit with popped zit and then add class fadeOutDown
+// when clicked, swap image of unpopped zit with popped zit and then add class fadeOutDown. after 2 seconds, remove() from document
+$(`.gameScreen`).on(`click`, `.newZit`, function() {
+    console.log('pop', this);
+    $(this).removeClass(`newZit`).addClass(`poppedZit animated fadeOut delay-1s`).attr(`src`, `./images/flat-zit-popped.png`);
+    setTimeout(() => {
+        $(this).remove();
+    }, 500)
+})
