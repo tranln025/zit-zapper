88a45f4838dfade9999c44402ceab95476ffc0e9
ltran <tranln025@gmail.com>
2019-10-02 22:33:29 -0700

    Replaces HTML canvas with div in which gameplay will occur

--- js/app.js
+++ js/app.js
@@ -1,51 +1,23 @@
-const canvas = document.getElementById('gameScreen');
-// getContext('2d') allows you to access the drawing context for 2D graphics
-const ctx = canvas.getContext('2d');
-
-// Fill at top of canvas, displays stopwatch and score counter
-ctx.fillStyle = 'rgb(255, 124, 110)';
-ctx.fillRect(0, 0, canvas.width, 50);
-
-// Stopwatch icon
-var stopwatch = new Image();
-stopwatch.addEventListener('load', function() {
-    ctx.drawImage(stopwatch, 10, 10, 27, 30);
-}, false);
-stopwatch.src = './images/stopwatch-solid.svg';
-    // stopwatch source: https://fontawesome.com/icons/stopwatch?style=solid
-
-// Countdown Text
-let timeLeft = 30;
-ctx.fillStyle = "white";
-ctx.font = "25px Gloria Hallelujah";
-ctx.fillText(timeLeft, 50, 35);
 
 // Timer Countdown Functions
 // Every second, reduce countdown by 1. When no time is left, stop the countdown, display score, offer "try again" button. On button click, restart countdown
+let timeLeft = 30;
 const startTimer = () => {
     const timer = setInterval(()=>{    // setInterval ( function, amount of time )
-        timeLeft--;
-        // Clear previous timer text and replace it each second with updated timeLeft
-        ctx.clearRect(40, 0, 50, 50);
-        ctx.fillStyle = 'rgb(255, 124, 110)';
-        ctx.fillRect(40, 0, 50, 50);
-        ctx.fillStyle = "white";
-        ctx.font = "25px Gloria Hallelujah";
-        ctx.fillText(timeLeft, 50, 35);
+        timeLeft --;
         if (timeLeft === 0) {
             clearInterval(timer);
+            // TODO insert "try again" button
         }
+        updateTime();
     }, 1000) 
-    return;
 }
 
-// Scoreboard
-let points = 0;
-ctx.fillStyle = "white";
-ctx.font = "25px Gloria Hallelujah";
-ctx.fillText("Points: " + points, 570, 35);
+const updateTime = () => {
+    $(`.countdown`).text(`${timeLeft} seconds`)
+}
 
-// When Start, remove instructions and start game
+// When Start is clicked, remove instructions and start game
 $(`.start`).on(`click`, () => {
     console.log(`START has been clicked`);
     $(`.instructionsContainer`).removeClass(`zoomIn delay-1s`).addClass(`zoomOut`);
@@ -53,39 +25,40 @@ $(`.start`).on(`click`, () => {
     startTimer();
 })
 
-// // Big Popup Zit
-// var bigZit = new Image();
-// bigZit.addEventListener('load', function() {
-//     console.log('test');
-//     ctx.drawImage(bigZit, 500, 300, 200, 100);
-// }, false);
-// bigZit.src = './images/big-zit.png';
 
-// // Small Popup Zit
-// var smallZit = new Image();
-// smallZit.addEventListener('load', function() {
-//     console.log('test');
-//     ctx.drawImage(smallZit, 500, 300, 100, 50);
-// }, false);
-// smallZit.src = './images/small-zit.png';
+// // Randomize zit position and time interval
+// const randomZitPos = () => {
+//     let x = Math.random() * ($(`.gameScreen`).width - 120) + 30;
+//     let y = Math.random() * ($(`.gameScreen`).height - 200) + 60;
+//     $(`.gameScreen`).append(`<img class="ripeZit" src="./images/flat-zit-unpopped.png" alt="ripe pimple">`);
+// };
 
-// Small Flat Zit
-var flatZit = new Image();
-// Function to draw the zit. Start button triggers drawing. 
-// ctx.drawImage(flatZit, 100, 100, 110, 90);
-flatZit.src = './images/flat-zit-unpopped.png';
+// const randomTimeInterval = (Math.random() * 5000) + 1000;
+// const zitPopUp = () => {
+//     setInterval(randomZitPos(), randomTimeInterval);
+// }
 
-// Randomize zit position
-const randomZit = () => {
-    let x = Math.random() * (canvas.width - 120) + 30;
-    let y = Math.random() * (canvas.height - 200) + 60;
-    flatZit.addEventListener('load', function() {
-        ctx.drawImage(flatZit, x, y, 110, 90);
-    }, false);
-};
+// Code for elements to appear sporadically and in various sizes. Source: http://jsfiddle.net/redler/QcUPk/8/
+function zitAppear(){
+    let divsize = (Math.round(Math.random()*100) + 50);
+    $newZit = $('<img class="newZit" src="./images/flat-zit-unpopped.png" alt="ripe pimple">').css({
+        'width':divsize+'px',
+        'height':divsize+'px',
+    });
+    
+    let posx = Math.random() * ($(`.gameScreen`).width() - divsize);
+    let posy = Math.random() * (($(`.gameScreen`).height() - divsize) - 50) + 50;
+    let randDelay = Math.random() * 20000;
+    
+    $newZit.css({
+        'position':'absolute',
+        'left':posx+'px',
+        'top':posy+'px',
+        'display':'none'
+    }).appendTo('.gameScreen').fadeIn().delay(randDelay); 
+}; 
 
-// in span of random time from 0 to 30 seconds, run random zit function
-const randomTimeInterval = (Math.random() * 5000) + 1000;
-const zitPopUp = setInterval(randomZit(), randomTimeInterval);
+zitAppear();
 
-// If zit clicked, change image to popped zit and then disappear
+
+// when clicked, swap image of unpopped zit with popped zit and then add class fadeOutDown
